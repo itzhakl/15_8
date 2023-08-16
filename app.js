3b4f665907bdbae481d6c74ce3e0ce42840c5cc6
itzhakl <il0556744424@gmail.com>
2023-08-16 14:49:28 +0300

    16_8

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ const { error } = require('console');
 const express = require('express');
 const app = express();
 const port = 3000;
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const users = [
-    { id: 1, email: 'user1@example.com', password: 'password1' },
-    { id: 2, email: 'user2@example.com', password: 'password2' },
-    { id: 3, email: 'user3@example.com', password: 'password3' },
-    { id: 4, email: 'user4@example.com', password: 'password4' }
+    { id: uuidv4(), email: 'user1@example.com', password: 'password1' },
+    { id: uuidv4(), email: 'user2@example.com', password: 'password2' },
+    { id: uuidv4(), email: 'user3@example.com', password: 'password3' },
+    { id: uuidv4(), email: 'user4@example.com', password: 'password4' }
 ];
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port:${port}`);
+});
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-
-
 
 app.get('/users', (req, res) => {
     res.send(users);
@@ -27,21 +32,57 @@ app.get('/users/:id', (req, res) => {
     res.send(users.filter(user => user.id === id)[0]);
 });
 
+
 app.post('/users', (req, res) => {
-    const Nuser = {
-        id: req.body.id,
-        email: req.body.email,
-        password: req.body.password
-    };
+    const pass = req.body.password
     users[users.length] = {
-        id: Nuser.id,
-        email: Nuser.email,
-        password: Nuser.password
+        id: uuidv4(),
+        email: req.body.email,
+        password: bcrypt.hash(pass, saltRounds)
     }
+    console.log(users);
     res.send("sucsess!!");
 });
 
+app.put('/users/:id', (req, res) => {
+    const idParam = +req.params.id;
+    const edit = req.body;
+    for (let user of users) {
+        if (user.id === idParam) {
+            if (req.body.email) user.email = edit.email;
+            if (req.body.password) user.password = edit.password;
+        }
+    }
+    res.send('sucsses!')
+    console.log(users);
+})
 
-app.listen(port, () => {
-    console.log(`Server is up and running on port:${port}`);
+app.delete('/users/:id', (req, res) => {
+    const idParam = +req.params.id;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === idParam) {
+            users.splice(i, 1);
+            res.send(`delete user ${idParam}`);
+        } else {
+            res.send('Id Not found');
+        }
+    }
+    console.log(users);
 });
+
+app.post('/users/verify', (req, res) => {
+    const mail = req.body.email;
+    const pass = req.body.password;
+    bcrypt.hash(pass, saltRounds (err, hash) => {
+        console.log(bcrypt.hash);
+    });
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === mail) {
+            bcrypt.compare(pass, users[i].password, (err, result) => {
+                if (err) throw error;
+                result ? res.send('User connected') : res.send('wrong password')
+            })
+        }
+    }
+    res.send('mail not found')
+})
